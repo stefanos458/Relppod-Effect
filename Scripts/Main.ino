fc9cc09b2f0a164d79a4a83f5f0f24849a43aba6
stefanos458 <46913489+stefanos458@users.noreply.github.com>
2020-07-16 19:47:01 -0400

    woops

---
+++ Scripts/Main.ino
@@ -0,0 +1,39 @@
+int piezoPin = 8;
+
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+  float vo = 100; // Velocity MPH
+  vo = vo*0.44704; // Velocity m/s
+
+  float d = 150; // run distance ft
+  d = d*0.3048; // run distance ft
+
+  float a = 343; // speed of sound m/s
+
+  float f_target = 200; // target frequency Hz
+  float f[] = {
+  (a - vo)/a*f_target,(a + vo)/a*f_target};
+  
+ 
+  float delta_t = d/vo*1000; // delay milliseconds
+  Serial.print("First Frequency (Hz): ");  
+  Serial.print(f[0]);       
+  Serial.print("\n");  
+  Serial.print("Second Frequency (Hz): ");  
+  Serial.print(f[1]);       
+  Serial.print("\n");  
+  Serial.print("Time Before Trigger (ms): ");  
+  Serial.print(delta_t);       
+  Serial.print("\n\n");  
+  for (int i = 0; i < 2; i++) {
+    tone(piezoPin,f[i],delta_t);
+    delay(delta_t);
+  }
+ 
+  
+}
+
+void loop() {
+ 
+}
