1943f37a92441e70ceb2c69c148ee7b3b77af258
stefanos458 <46913489+stefanos458@users.noreply.github.com>
2020-07-16 20:23:13 -0400

    Added Comments

--- Scripts/Main/Main.ino
+++ Scripts/Main/Main.ino
@@ -12,18 +12,18 @@ void setup() {
   float D = 20; // Standoff Distance ft
   D = D*0.3048; // Standoff Distance m
 
-  float a = 343; // speed of sound m/s
-  float f_target = 200; // target frequency Hz
+  float a = 343; // Speed of sound m/s
+  float f_target = 200; // Target frequency Hz
   
   //for () {
-    float rel_v[] = -vo*(d - t*vo)/sqrt(d-t*vo)^2 + D^2);
-    float f[] = (a + rel_v)/a*f_target;
+    float rel_v[] = -vo*(d - t*vo)/sqrt(d-t*vo)^2 + D^2); // Relative velocity 
+    float f[] = (a + rel_v)/a*f_target; // Adjusted frequency based on relative velocity
   //}
   
   
   
   
-  float delta_t = d/vo*1000; // delay milliseconds before source passes observer (Not useful rn i think)
+  float delta_t = d/vo*1000; // Delay milliseconds before source passes observer (Not useful rn i think)
   
   Serial.print("First Frequency (Hz): ");  
   Serial.print(f[0]);       
