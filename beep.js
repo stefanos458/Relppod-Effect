0a1cf0d4a693539081bace7f5926066ef4611b33
sofieg <sofiegonzalez98@gmail.com>
2020-11-03 11:38:16 -0500

    js oscilator

---
+++ beep.js
@@ -0,0 +1,11 @@
+// create web audio api context
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+// create Oscillator node
+var oscillator = audioCtx.createOscillator();
+var gainNode = audioCtx.createGain();
+
+oscillator.type = 'square';
+oscillator.frequency.setValueAtTime(440, audioCtx.currentTime); // value in hertz
+oscillator.connect(audioCtx.destination);
+oscillator.start();
